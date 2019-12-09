dd12e5e050e87c065f5dc25d5d3ad3a7fdc0942b
Joanne G. Basa <joannegbasa@gmail.com>
2019-12-09 12:40:36 +0000

    added App component and changed page title

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
 
-render(<div>Hello World!</div>, global.document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
